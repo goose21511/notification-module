bc8e1fbf0b6ca92c3872705fca63f3996a4a2e6e
goose21511 <goosi3st2151@gmail.com>
2026-01-16 20:57:50 +0100

    Fix comment formatting and improve notification example
    Fixed formatting

--- example.lua
+++ example.lua
@@ -10,7 +10,8 @@ uis.InputBegan:Connect(function(i, e)
 	end
 end)
 
---// Scripted notification exampleuis.InputBegan:Connect(function(i, e)
+--// Scripted notification example
+uis.InputBegan:Connect(function(i, e)
 	if e then return end
 	if i.KeyCode == Enum.KeyCode.G then
 		notif:CreateNotification("Info", "Simply an example!", 2, print("Hello")) --// The first string is the notification type, choose one from the module or make your own, the second string is your message, and the last is a number where you can choose how long it lasts. Then we have the optional function, where you can make it run code or a function.
